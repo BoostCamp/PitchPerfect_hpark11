682ab9e3bf06e061e2bb78c63fc37c85872f6670
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-09 02:53:58 +0900

    Enable record, stop, pause and resume

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -2,13 +2,16 @@
 //  ViewController.swift
 //  PitchPerfect
 //
-//  Created by connect on 2017. 1. 9..
+//  Created by hpark on 2017. 1. 9..
 //  Copyright © 2017년 boostcamp. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class RecordSoundsViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
     
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
@@ -27,19 +30,52 @@ class RecordSoundsViewController: UIViewController {
     
     @IBAction func recordAudio(_ sender: Any) {
         changeUIState(record: false, stop: false, pause: false, resume: true, recordLbl: false, state: "Recording")
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with:AVAudioSessionCategoryOptions.defaultToSpeaker)
+            try audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        } catch _ {
+            print("::: Error in Recording Audio")
+        }
+    
+        audioRecorder.delegate = self
+        audioRecorder.isMeteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     @IBAction func stopRecording(_ sender: Any) {
         changeUIState(record: true, stop: true, pause: true, resume: true, recordLbl: true, state: "")
+        
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setActive(false)
+        } catch _ {
+            print("::: Error in Stop Recording")
+        }
+        
     }
     
     @IBAction func pauseRecording(_ sender: Any) {
         changeUIState(record: false, stop: false, pause: true, resume: false, recordLbl: false, state: "Recording Paused")
+        
+        audioRecorder.pause()
     }
     
     @IBAction func resumeRecording(_ sender: Any) {
         changeUIState(record: false, stop: false, pause: false, resume: true, recordLbl: false, state: "Recording")
+        
+        audioRecorder.record()
     }
+    
+    
     
     // MARK: General methods
     
