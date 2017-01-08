adfe6dc53c107a8d6b2b9bc10091e746ee655592
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-09 02:36:20 +0900

    Changing state of buttons in RecordSoundsVC
    Also added pause and resume recording

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -9,9 +9,66 @@
 import UIKit
 
 class RecordSoundsViewController: UIViewController {
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var pauseRecordingButton: UIButton!
+    @IBOutlet weak var resumeRecordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    // MARK: Lifecycle methods override
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        changeUIState(record: true, stop: true, pause: true, resume: true, recordLbl: true, state: "")
     }
+    
+    // MARK: Actions Relating to Audio Recording
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        changeUIState(record: false, stop: false, pause: false, resume: true, recordLbl: false, state: "Recording")
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        changeUIState(record: true, stop: true, pause: true, resume: true, recordLbl: true, state: "")
+    }
+    
+    @IBAction func pauseRecording(_ sender: Any) {
+        changeUIState(record: false, stop: false, pause: true, resume: false, recordLbl: false, state: "Recording Paused")
+    }
+    
+    @IBAction func resumeRecording(_ sender: Any) {
+        changeUIState(record: false, stop: false, pause: false, resume: true, recordLbl: false, state: "Recording")
+    }
+    
+    // MARK: General methods
+    
+    func changeUIState(record:Bool, stop:Bool, pause:Bool, resume:Bool, recordLbl: Bool, state:String? = nil) {
+        
+        if recordButton.isEnabled != record {
+            recordButton.isEnabled = record
+        }
+        
+        if stopRecordingButton.isHidden != stop {
+            stopRecordingButton.isHidden = stop
+        }
+        
+        if pauseRecordingButton.isHidden != pause {
+            pauseRecordingButton.isHidden = pause
+        }
+        
+        if resumeRecordingButton.isHidden != resume {
+            resumeRecordingButton.isHidden = resume
+        }
+        
+        if recordingLabel.isHidden != recordLbl {
+            recordingLabel.isHidden = recordLbl
+        }
+        
+        if let state = state {
+            recordingLabel.text = state
+        }
+    }
+    
 }
 
