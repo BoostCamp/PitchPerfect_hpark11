cf693928b1d7bb8df6b5a8e25567289ee9414d9d
HyunsooPark <qkrgustnrk@gmail.com>
2017-01-11 18:19:01 +0900

    Changed structure of the entire project

---
+++ PitchPerfect/SoundsListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SoundsListViewController.swift
+//  PitchPerfect
+//
+//  Created by connect on 2017. 1. 11..
+//  Copyright © 2017년 boostcamp. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SoundsListViewController: UIViewController {
+    
+    @IBOutlet weak var soundsTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ PitchPerfect/SoundsListViewController_ex.swift
@@ -0,0 +1,14 @@
+//
+//  SoundsListViewController_ex.swift
+//  PitchPerfect
+//
+//  Created by connect on 2017. 1. 11..
+//  Copyright © 2017년 boostcamp. All rights reserved.
+//
+
+import UIKit
+
+extension SoundsListViewController {
+    
+}
+
